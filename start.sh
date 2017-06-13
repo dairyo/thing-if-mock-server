6b45a89081859568045e1b27571948ffc43cf2f2
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-13 15:57:19 +0900

    Add scripts.

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+java -jar wiremock-standalone-2.6.0.jar --port 1080 &
+
+sleep 5
+
+curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/get_commannd.json
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/shutdown
