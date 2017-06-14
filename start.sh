fecbfe291a716c739687978e6e7a14db15b87bb8
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-14 13:54:19 +0900

    Add expectations for posting new command.

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ nohup java -jar wiremock-standalone-2.6.0.jar --port 1080 &
 sleep 5
 
 curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/get_commannd.json
+curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/post_command_1.json
+curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/post_command_2.json
