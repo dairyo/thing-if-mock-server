9d333351f7f2afc7607f3947eff2e7c4c13e1620
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-15 16:35:14 +0900

    Add an expectation for onboarding with thing id 403.

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ nohup java -jar wiremock-standalone-2.6.0.jar --port 1080 &
 sleep 5
 
 curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/onboard-with-thing-id-success.json
+curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/onboard-with-thing-id-403.json
