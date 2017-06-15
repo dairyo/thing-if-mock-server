caa4066cd0b84ddf7e44fbe1bbecf67d57284a12
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-15 16:51:58 +0900

    Add an expectation for onboarding with thing id 500.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/onboard-with-thing-id-success.json
 curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/onboard-with-thing-id-403.json
 curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/onboard-with-thing-id-404.json
+curl -X POST -H "Content-Type: application/json" http://localhost:1080/__admin/mappings -d @expectations/onboard-with-thing-id-500.json
